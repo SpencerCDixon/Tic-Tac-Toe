3c75a202a6ae8bc7e46462dd81e4b133864b5374
Spencer Dixon <spencercdixon@gmail.com>
2014-08-06 22:30:32 -0400

    Organized code up a bit

--- play_game.rb
+++ play_game.rb
@@ -1,19 +1,5 @@
-# require 'pp'
-# line = %w{------------------------}
-# a_row = ["A1", "|", "A2", "|", "A3"]
-# b_row = ["B1", "|", "B2", "|", "B3"]
-# c_row = ["C1", "|", "C2", "|", "C3"]
-#
-# gameboard = [a_row, line, b_row, line, c_row]
-#
-# gameboard.each do |row|
-#   p row
-# end
-
 require './gameboard.rb'
 require './player.rb'
-
-
 
 board = Gameboard.new
 
@@ -27,6 +13,7 @@ p1.id = 'X'
 p2 = Player.new(player_2_name)
 p2.id = 'O'
 puts
+
 while true
 #Player 1 turn
 p1.player_turn(board)
@@ -39,17 +26,3 @@ board.take_turn(p2.number,p2)
 board.check_for_win(p2)
 puts
 end
-
-
-
-
-# board.show_board
-# puts
-# puts
-# board.gameboard[0].default_mark = 'X'
-# board.gameboard[1].default_mark = 'X'
-# board.gameboard[2].default_mark = 'X'
-# board.show_board
-#
-#
-# board.check_for_win
